4ac5768e94291805c986bfbc68b8f8783ce007f7
Pratyasa Roy <proy8892@conestogac.on.ca>
2026-07-24 16:19:42 -0400

    Implemented initial main.c

---
+++ Assignment2/Assignment2/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "bst.h"
+
+int main(void)// Main function
+{
+	TreeNode* root = NULL;// Initialize the root of the binary search tree to NULL
+	int numberOfLetters;// Variable to store the number of random letters to generate
+	int i;// Variable for loop iteration
+	char randomLetter;// Variable to store the randomly generated letter
+
+	srand((unsigned int)time(NULL));// Seed the random number generator with the current time
+
+	numberOfLetters = 11 + rand() % 10;// Generate a random number between 11 and 20
+
+	printf("Number of random letters: %d\n", numberOfLetters);// Print the number of random letters to be generated
+	printf("Generated letters: ");// Print the generated letters
+
+	for (i = 0; i < numberOfLetters; i++)// Loop to generate random letters
+    {
+		randomLetter = 'a' + rand() % 26;// Generate a random letter between 'a' and 'z'
+
+		printf("%c ", randomLetter);// Print the generated letter
+
+		root = insertNode(root, randomLetter);// Insert the generated letter into the binary search tree
+    }
+
+	printf("\n");// Print a newline after the generated letters
+
+    return 0;
+}
