46275cf1980df006fccf1ad7bf502a99e55980e3
Pratyasa Roy <proy8892@conestogac.on.ca>
2026-07-24 15:11:05 -0400

    Written insert() function

---
+++ Assignment2/Assignment2/bst.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "bst.h"
+
+TreeNode* insertNode(TreeNode* root, char value)// Function to insert a node into the binary search tree
+{
+	if (root == NULL)// If the tree is empty, create a new node
+	{
+		TreeNode* newNode =
+			(TreeNode*)malloc(sizeof(TreeNode));// Allocate memory for a new node
+
+		if (newNode == NULL)// Check if memory allocation was successful
+		{
+			printf("Memory allocation failed.\n");// Print an error message
+			return NULL;// Return NULL if memory allocation failed
+		}
+
+		newNode->data = value;// Set the data of the new node
+		newNode->left = NULL;// Set the left child of the new node to NULL
+		newNode->right = NULL;//Set the right child of the new node to NULL
+
+		return newNode;// Return the new node
+	}
+
+	if (value < root->data)// If the value is less than the root's data, insert it in the left subtree
+	{
+		root->left = insertNode(root->left, value);// Recursively insert the value in the left subtree
+	}
+	else
+	{
+		root->right = insertNode(root->right, value);// Recursively insert the value in the right subtree
+	}
+
+	return root;// Return the root of the tree
+}
+
